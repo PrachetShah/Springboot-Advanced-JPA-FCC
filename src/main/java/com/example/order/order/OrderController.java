ea8df29390d2f11399c9592f729a55bbf2c0a957
PrachetShah <prachetshah25@gmail.com>
2024-05-02 18:22:45 +0530

    created post request and save in repository component

--- src/main/java/com/example/order/order/OrderController.java
+++ src/main/java/com/example/order/order/OrderController.java
@@ -1,12 +1,31 @@
 package com.example.order.order;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+    private final OrderRepository orderRepo;
+    public OrderController(OrderRepository orderRepo){
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping
-    public String hello(){
-        return "Hello Order Application World";
+    public List<Order> hello(){
+        return orderRepo.findAll();
+    }
+
+    @PostMapping
+    public Order add(@RequestBody Order order){
+        return orderRepo.save(order);
+    }
+
+    @GetMapping(path="api/orders")
+    public List<String> hi(){
+        return List.of("Prachet", "Kevin");
     }
 }
